e07d4becf444b362644c579c4686343da5365f14
elb7007 <elb7007>
2019-10-15 15:55:28 -0400

    Update the plugin

--- JSONTransformer/JSONTransformer_ctrlr.js
+++ JSONTransformer/JSONTransformer_ctrlr.js
@@ -2,12 +2,9 @@
  * @projectDescription	Displays demographic information for a single patient set.
  * @inherits	i2b2
  * @namespace	i2b2.JSONTransformer
- * @author		Nick Benik, Griffin Weber MD PhD
- * @version 	1.4
+ * @author		
+ * @version 	1.0
  * ----------------------------------------------------------------------------------------
- * 2008-12-22:  Initial Launch [Griffin Weber] 
- * 2015-10-26:  Fixed issue with JSON not working [Mike Mendis]
- * 2015-12-30:  Fixed missing 'Marital Status' histogram issue (JIRA PLUGINS-10) [Wayne Chan]      
  */
 
 i2b2.JSONTransformer.Init = function(loadedDiv) {
@@ -51,7 +48,7 @@ i2b2.JSONTransformer.qmDropped = function(sdxData) {
 	// save the info to our local data model
 	i2b2.JSONTransformer.model.qmRecord = sdxData;
 	// let the user know that the drop was successful by displaying the name of the patient set
-	$("JSONTransformer-QMDROP").innerHTML = i2b2.h.Escape(sdxData.sdxInfo.sdxDisplayName);
+	$("JSONTransformer-QMDROP").innerHTML = i2b2.h.Escape(sdxData.sdxInfo.sdxDisplayName+"[QMid="+sdxData.sdxInfo.sdxKeyValue+"]");
 	// temporarly change background color to give GUI feedback of a successful drop occuring
 	$("JSONTransformer-QMDROP").style.background = "#CFB";
 	setTimeout("$('JSONTransformer-QMDROP').style.background='#DEEBEF'", 250);	
@@ -60,139 +57,80 @@ i2b2.JSONTransformer.qmDropped = function(sdxData) {
 };
 
 i2b2.JSONTransformer.getResults = function() {
-	if (i2b2.JSONTransformer.model.dirtyResultsData) {
-
-		var msg_filter = '<input_list>\n' +
-			'	<patient_list max="99999" min="0">\n' +
-			'		<patient_set_coll_id>'+i2b2.JSONTransformer.model.qmRecord.sdxInfo.sdxKeyValue+'</patient_set_coll_id>\n'+
-			'	</patient_list>\n'+
-			'</input_list>\n'+
-			'<filter_list />\n'+
-			'<output_option>\n'+
-			'	<patient_set select="using_input_list" onlykeys="false"/>\n'+
-			'</output_option>\n';
-
-		// callback processor
-		var scopedCallback = new i2b2_scopedCallback();
-		scopedCallback.scope = this;
-		scopedCallback.callback = function(results) {
-			// THIS function is used to process the AJAX results of the getChild call
-			//		results data object contains the following attributes:
-			//			refXML: xmlDomObject <--- for data processing
-			//			msgRequest: xml (string)
-			//			msgResponse: xml (string)
-			//			error: boolean
-			//			errorStatus: string [only with error=true]
-			//			errorMsg: string [only with error=true]
-			
-			// check for errors
-			if (results.error) {
-				alert('The results from the server could not be understood.  Press F12 for more information.');
-				console.error("Bad Results from Cell Communicator: ",results);
-				return false;
-			}
+	if (!(i2b2.JSONTransformer.model.dirtyResultsData)) return false; 
+        var reqURL= "http://i2b2app.sis.nyp.org:9088/JSONServlet&QMid="+i2b2.JSONTransformer.model.qmRecord.sdxInfo.sdxKeyValue;
 
 			$$("DIV#JSONTransformer-mainDiv DIV#JSONTransformer-TABS DIV.results-working")[0].hide();			
 			$$("DIV#JSONTransformer-mainDiv DIV#JSONTransformer-TABS DIV.results-finished")[0].show();
 
 
-			// get all the patient records
-			var pData = i2b2.h.XPath(results.refXML, 'descendant::patient/param[@column]/text()/..');
-			var hData = new Hash();
-			for (var i1=0; i1<pData.length; i1++) {
-				var n = pData[i1].getAttribute('column');
-				var t1 = hData.get(n);
-				if (!t1) { t1 = new Hash(); }
-				var v = pData[i1].firstChild.nodeValue;
-				if (n=="birth_date") {
-					v = v.substring(0, v.indexOf("T"));
-				}
-				if (n=="age_in_years_num") {
-					v = Math.floor(v/10);
-					if (v<0) {v=0;}
-					if (v>9) {v=9;}
-					if (v==0) {v='0-10';} else {v=v+'0-'+(v+1)+'0';}
-				}
-				var t2 = t1.get(v);
-				if (!t2) {
-					t2 = 1;
-				} else {
-					t2++;
-				}
-				t1.set(v, t2);
-				hData.set(n, t1);
-			}
-			
-			// collapse the hash objects to regular objects and save to the Plugin's data model
-			i2b2.JSONTransformer.model.sumCounts = eval("(" + Object.toJSON(hData) +")"); 
 			var s = '';
 			
 			s += '<div class="intro">';
-			s += 'Below are the demographic details for the selected patient set. ';
-			s += 'For each demographic category, the values, number of patients, and a histogram are shown.';
+			s += 'JSON transformation of request_xml query definition of QM previous query. ';
 			s += '</div>';
 
 			s += '<div class="resultLV">';
-			s += '<div class="resultLbl">Patient Set:</div>';
+			s += '<div class="resultLbl">Query definition:</div>';
 			s += '<div class="resultVal">' + i2b2.JSONTransformer.model.qmRecord.sdxInfo.sdxDisplayName + '</div>';
 			s += '</div>'
+                      
+                        s += '<div class="resultLV">';
+                        s += '<div class="resultLbl">Query_Master_id:</div>';
+                        s += '<div class="resultVal">' + i2b2.JSONTransformer.model.qmRecord.sdxInfo.sdxKeyValue + '</div>';
+                        s += '</div>'
 	
 			s += '<div class="resultLV">';
-			s += '<div class="resultLbl">Patient Count:</div>';
-			s += '<div class="resultVal">' + i2b2.JSONTransformer.model.qmRecord.origData.size + '</div>';
+			s += '<div class="resultLbl">URL:</div>';
+			s += '<div class="resultVal">'+reqURL+ '</div>';
 			s += '</div>'
 
+                        s += '<br/>';
+
+			s += '<div class="resultLbl">Servlet:</div>';
+   //                     s += '<div class="resultVal">'+ajaxSyncRequest(encodeURIComponent(reqURL))+ '</div>';
+                        s += '</div>'
 			
-			s += '<div class="demTables">';
-			var DemCats = {age_in_years_num:'Age in Years', 
-					sex_cd:'Sex',
-					race_cd:'Race',
-					language_cd:'Language',
-					marital_status_cd:'Marital Status',
-					religion_cd:'Religion',
-					vital_status_cd:'Vital Status (Deceased)'
-				};
-			for (var DemCat in DemCats) {
-				var DemCatVals = [];
-				var maxVal = 0;
-				for (var DemCatVal in i2b2.JSONTransformer.model.sumCounts[DemCat]) {
-					var tempVal = i2b2.JSONTransformer.model.sumCounts[DemCat][DemCatVal];
-					DemCatVals.push( [DemCatVal, tempVal] );
-					//if (tempVal > maxVal) {maxVal = tempVal;}
-					maxVal += tempVal;
-				}
-				maxVal *= 1.0;
-				if (maxVal > 0) {
-					s += '<div class="demcatTitle">' + DemCats[DemCat] + '</div>';
-					s += '<table>';
-					DemCatVals.sort();
-					//DemCatVals.sort(function() {return arguments[0][0] > arguments[1][0]});
-					for (i1=0; i1<DemCatVals.length; i1++) {
-						var barWidth = 200 * (DemCatVals[i1][1]/maxVal);
-						s += '<tr>';
-						s += '<th>' + DemCatVals[i1][0] + '</th>';
-						s += '<td>' + DemCatVals[i1][1] + '</td>';
-						s += '<td class="barTD"><div class="bar" style="width:' + barWidth + 'px;"></div></td>';
-						s += '</tr>';
-					}
-					s += '</table>';
-				}
-			}
-			s += '</div>';
 			
 			s += '<br/>';
 
-			$$("DIV#JSONTransformer-mainDiv DIV#JSONTransformer-TABS DIV.results-finished")[0].innerHTML = s;
+			$$("DIV#JSONTransformer-mainDiv DIV#JSONTransformer-TABS DIV.results-working")[0].innerHTML = s;
 
 			// optimization - only requery when the input data is changed
 			i2b2.JSONTransformer.model.dirtyResultsData = false;		
-		}
 		
 		$$("DIV#JSONTransformer-mainDiv DIV#JSONTransformer-TABS DIV.results-directions")[0].hide();
 		$$("DIV#JSONTransformer-mainDiv DIV#JSONTransformer-TABS DIV.results-finished")[0].hide();
 		$$("DIV#JSONTransformer-mainDiv DIV#JSONTransformer-TABS DIV.results-working")[0].show();		
 		
 		// AJAX CALL USING THE EXISTING CRC CELL COMMUNICATOR
-		i2b2.CRC.ajax.getPDO_fromInputList("Plugin:JSONTransformer", {PDO_Request: msg_filter}, scopedCallback);
-	}
+        ajaxSyncRequest(reqURL);
+	
+}
+
+function ajaxSyncRequest(reqURL) {
+    //Creating a new XMLHttpRequest object
+    var xmlhttp;
+    if (window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest(); //for IE7+, Firefox, Chrome, Opera, Safari
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); //for IE6, IE5
+    }
+    //Create a asynchronous GET request
+  //  var n= location.search.indexOf("token=")+6;
+  //  reqURL=reqURL+"/?QMid="+encodeURIComponent(location.search.substr(n)); //requrl='/ShrineServlet/'+ location.search;
+    xmlhttp.open("GET", reqURL, false);
+    xmlhttp.send(null);
+     
+    //Execution blocked till server send the response
+   if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200)
+        { // document.getElementById("message").innerHTML =
+ return xmlhttp.responseText;
+        }
+        else
+        {
+            alert('Something is wrong !!');
+        }
+    }
 }
